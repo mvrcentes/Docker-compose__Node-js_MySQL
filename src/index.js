939a7c752bf6e39a25eaef5a951bf31e7cd5d5c9
mvrcentes <mvrcentes@hotmail.com>
2023-06-28 14:41:35 -0600

    Add dotenv

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import express from "express"
 import { createPool } from "mysql2/promise"
+import { config } from "dotenv"
+config()
 
 const app = express()
 
 const pool = createPool({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "test",
-    port: 3306,
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQLDB_USER,
+    password: process.env.MYSQLDB_PASSWORD,
+    database: process.env.MYSQLDB_DB,
+    port: process.env.MYSQLDB_PORT,
 })
 
 app.get("/", (req, res) => {
@@ -20,6 +22,6 @@ app.get("/ping", async (req, res) => {
     res.json(result[0])
 })
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000")
+app.listen(process.env.NODE_DOCKER_PORT, () => {
+    console.log("Server is running on port", process.env.NODE_DOCKER_PORT)
 })
